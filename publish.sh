bdd2431281f4fe14962d4a1936d4aef9c1d7cd41
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-18 10:53:55 +0000

    updated shell script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
 rm index.zip
-zip -r "index.zip" Package
+cd package
+zip -X -r "../index.zip" *
+cd ..
 aws lambda update-function-code --function-name CreateThumbnailFromImage --zip-file fileb://index.zip
