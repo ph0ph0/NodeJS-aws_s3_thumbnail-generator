dd12a6275b0de9a51cef50629ed0a53df42f0fb2
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-18 22:41:22 +0000

    converted from gm to sharp

--- Package/index.js
+++ Package/index.js
@@ -1,6 +1,5 @@
 const AWS = require("aws-sdk");
-const gm = require("gm").subClass({ imageMagick: true });
-const async = require("async");
+const sharp = require("sharp");
 
 const s3 = new AWS.S3();
 
@@ -13,15 +12,10 @@ exports.handler = async (event, context) => {
 
   const sourceBucket = event.Records[0].s3.bucket.name;
   const sourceKey = event.Records[0].s3.object.key;
-
   const lastSlash = sourceKey.lastIndexOf("/");
   const fileName = sourceKey.substring(lastSlash);
   const destinationBucket = sourceBucket + "/subjectPictures-thumbnails/";
   const destinationKey = "public/subjectPictures-thumbnails" + fileName;
-
-  const lastDot = fileName.lastIndexOf(".");
-  const fileType = fileName.substring(lastDot);
-  console.log("fileType: " + fileType);
 
   //Sanity check: ensure source and destination are different
   if (sourceBucket == destinationBucket) {
@@ -29,61 +23,60 @@ exports.handler = async (event, context) => {
     throw error;
   }
 
-  async.waterfall(
-    [
-      function downloadImage(callback) {
-        s3.getObject({ Bucket: sourceBucket, Key: sourceKey }, callback);
-      },
-      function transform(response, callback) {
-        gm(response.body).size((error, size) => {
-          console.log("Size of image: " + size);
+  try {
+    const uploadedImage = await s3
+      .getObject({ Bucket: sourceBucket, Key: sourceKey })
+      .promise();
 
-          const scalingFactor = Math.min(
-            MAX_HEIGHT / size.height,
-            MAX_WIDTH / size.width
-          );
-          const width = scalingFactor * size.width;
-          const height = scalingFactor * size.height;
+    // console.log("Uploaded image body: " + uploadedImage);
 
-          console.log(
-            "Scaling factor: %j",
-            scalingFactor,
-            " width: %j",
-            width,
-            " height: %j",
-            height
-          );
-
-          this.resize(width, height).toBuffer(fileType, (error, buffer) => {
-            console.log("resized image, now buffering...");
-            if (error) {
-              callback(error);
-            } else {
-              callback(null, response.ContentType, buffer);
-            }
-          });
+    sharp(uploadedImage.Body)
+      .resize(MAX_WIDTH, MAX_HEIGHT, {
+        fit: "contain"
+      })
+      .toBuffer()
+      .then(data => {
+        s3.putObject({
+          Bucket: destinationBucket,
+          Key: destinationKey,
+          Body: data
         });
-      },
-      function upload(contentType, data, callback) {
-        console.log("uploading to S3...");
-        s3.putObject(
-          {
-            Bucket: destinationBucket,
-            Key: destinationKey,
-            Body: data,
-            ContentType: contentType
-          },
-          callback
-        );
-      }
-    ],
-    function(err) {
-      if (err) {
-        console.log("Error in waterfall: " + error);
-      } else {
-        console.log("Successfully created thumbnail");
-      }
-      callback(null, message);
-    }
-  );
+      })
+      .then(() => {
+        console.log("Success!");
+      });
+
+    // const imageSize = await gm(uploadedImage.body).size((error, size) => {
+    //   console.log("width: " + size.width + " height: " + size.height);
+    //   const scalingFactor = Math.min(
+    //     MAX_HEIGHT / size.height,
+    //     MAX_WIDTH / size.width
+    //   );
+    //   const width = scalingFactor * size.width;
+    //   const height = scalingFactor * size.height;
+
+    //   console.log(
+    //     "Scaling factor: %j",
+    //     scalingFactor,
+    //     " width: %j",
+    //     width,
+    //     " height: %j",
+    //     height
+    //   );
+    //   return width;
+    // });
+    // console.log("imageSize: %j", +imageSize);
+
+    // const imageBuffer = gm(resizedImage).toBuffer;
+    // const thumbnailImage = await s3.putObject({
+    //   Bucket: destinationBucket,
+    //   Key: destinationKey,
+    //   Body: imageBuffer
+    // });
+    // console.log("Successfully wrote image, path: %j", thumbnailImage);
+    // return thumbnailImage;
+  } catch (error) {
+    console.log("ERROR!: " + error);
+    throw error;
+  }
 };
