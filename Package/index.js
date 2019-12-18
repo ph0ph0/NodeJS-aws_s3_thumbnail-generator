d1490333be52c01cb9c1d1ceb2b6f53e018bd720
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-18 11:34:59 +0000

    set up main index.js

--- Package/index.js
+++ Package/index.js
@@ -1,1 +1,10 @@
-var fromCLI = "";
+const AWS = require("aws-sdk");
+const gm = require("gm").subClass({ imageMagick: true });
+
+const s3 = new AWS.S3();
+
+//Constants
+const MAX_WIDTH = 175;
+const MAX_HEIGHT = 123;
+
+exports.handler = (event, context) => {};
