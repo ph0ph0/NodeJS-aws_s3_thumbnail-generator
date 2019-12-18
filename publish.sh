40d37e09cc65cc0f48375ace4450d9259a98cd45
ph0ph0 <phillipalanlee@googlemail.com>
2019-12-18 10:41:44 +0000

    updated the script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,3 @@
 rm index.zip
-cd Package
-zip –X –r ../index.zip *
-cd ..
-aws lambda update-function-code --function-name ConvertImageToThumbnail --zip-file fileb://index.zip
+zip -r "index.zip" Package
+aws lambda update-function-code --function-name CreateThumbnailFromImage --zip-file fileb://index.zip
